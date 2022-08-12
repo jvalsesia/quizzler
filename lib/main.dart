c74fbb3f4b53de4cf596ab3a1e7d41306e1581e6
jvalsesia <jvalsesia@gmail.com>
2022-08-12 14:38:36 -0300

    updating texts

--- lib/main.dart
+++ lib/main.dart
@@ -36,30 +36,30 @@ class _QuizPageState extends State<QuizPage> {
   int questionNumber = 0;
   List<Column> scoreKeeper = [];
   List<Question> questions = [
-    Question('Some cats are actually allergic to humans', true),
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
-    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question('Some cats are actually allergic to humans!', true),
+    Question('You can lead a cow down stairs but not up stairs!', false),
+    Question('Approximately one quarter of human bones are in the feet!', true),
+    Question('A slug\'s blood is green!', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\"!', true),
+    Question('It is illegal to pee in the Ocean in Portugal!', true),
     Question(
-        'No piece of square dry paper can be folded in half more than 7 times.',
+        'No piece of square dry paper can be folded in half more than 7 times!',
         false),
     Question(
-        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place!',
         true),
     Question(
-        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        'The loudest sound produced by any animal is 188 decibels! That animal is the African Elephant!',
         false),
     Question(
-        'The total surface area of two human lungs is approximately 70 square metres.',
+        'The total surface area of two human lungs is approximately 70 square metres!',
         true),
-    Question('Google was originally called \"Backrub\".', true),
+    Question('Google was originally called \"Backrub\"!', true),
     Question(
-        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog!',
         true),
     Question(
-        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat!',
         true),
   ];
 
@@ -208,7 +208,7 @@ class _QuizPageState extends State<QuizPage> {
                           'Score',
                           style: TextStyle(
                             fontSize: 20.0,
-                            color: Colors.white,
+                            color: Colors.yellow,
                           ),
                         ),
                         Text(
